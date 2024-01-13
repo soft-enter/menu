b5336d29f019d9f49fc790f642da6cea6e924e45
Mizael Soler <misaellsoler@gmail.com>
2024-01-13 01:44:53 -0400

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,21 @@ import { Menu } from "./components/Menu";
 
 export default function Page() {
   return (
-    <div className="bg-zinc-100 h-screen">
-      <Menu />
+    <div className="bg-red-200">
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+          backgroundImage: `url('/img/logo.jpg')`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Menu />
+      </div>
+      <div style={{ height: "100vh" }}></div>
+      <div style={{ height: "100vh" }}>seccion 3</div>
     </div>
   );
 }
