070eee92ad0663b3dcdb394e12eaf625883762da
Mizael <misaellsoler@gmail.com>
2024-01-16 02:35:55 -0400

    WIP cartas

--- app/carta/page.tsx
+++ app/carta/page.tsx
@@ -27,25 +27,30 @@ export default function Page() {
       <div className="bg-red-500">
         <Menu />
       </div>
-      <div className="flex justify-between mt-10 px-10">
-        {[firstColumn, secondColumn, thirdColumn].map((column, index) => (
-          <table key={index} className="w-1/3 mt-10">
-            <thead>
-              <tr>
-                <th className="text-xl font-bold pr-10 text-left">Nombre</th>
-                <th className="text-xl font-bold text-left">Precio</th>
-              </tr>
-            </thead>
-            <tbody>
-              {column.map((item) => (
-                <tr key={item.id}>
-                  <td className="text-md pr-10 font-bold py-4">{item.name}</td>
-                  <td className="py-4 text-sm">{`RD$ ${item.price}`}</td>
+      <div className="flex flex-col justify-center mt-10 px-10">
+        <h1 className="text-3xl font-bold mb-5 text-center">Desayunos</h1>
+        <div className="flex justify-between">
+          {[firstColumn, secondColumn, thirdColumn].map((column, index) => (
+            <table key={index} className="w-1/3 mt-10">
+              <thead>
+                <tr>
+                  <th className="text-xl font-bold pr-10 text-left">Nombre</th>
+                  <th className="text-xl font-bold text-left">Precio</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        ))}
+              </thead>
+              <tbody>
+                {column.map((item) => (
+                  <tr key={item.id}>
+                    <td className="text-md pr-10 font-bold py-4">
+                      {item.name}
+                    </td>
+                    <td className="py-4 text-sm">{`RD$ ${item.price}`}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ))}
+        </div>
       </div>
     </div>
   );
